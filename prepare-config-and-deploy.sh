6e90ce1529545ef00d96c71d744dd6b776cbcb97
seanlabor <50588881+seanlabor@users.noreply.github.com>
2025-04-27 22:12:21 +0200

    a

--- prepare-config-and-deploy.sh
+++ prepare-config-and-deploy.sh
@@ -1,9 +1,23 @@
 #!/bin/bash
 
 echo ">> Preparing SolarGather config.yaml with environment variables..."
-export $(cat .env | xargs) # lädt alle Variablen
+export $(cat .env | xargs)
 envsubst < solargather_SG12RG/config.template.yaml > solargather_SG12RG/config.yaml
 envsubst < solargather_SG4RG/config.template.yaml > solargather_SG4RG/config.yaml
+
+line65=$(sed -n '38p' solargather_SG12RG/config.yaml)
+echo "THis should contain a full url: $line65"
+
+
+# ensure all host config dirs exist
+for d in homeassistant influxdb mosquitto solargather_SG4RG solargather_SG12RG; do
+  DIR="/docker-static-config-data/$d"
+  if [ ! -d "$DIR" ]; then
+    echo "Creating host directory $DIR"
+    mkdir -p "$DIR"
+    chown florian:florian "$DIR"
+  fi
+done
 
 echo "🚀 Bringing up Docker containers..."
 docker compose up -d
