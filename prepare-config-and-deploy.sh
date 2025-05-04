1ca0c7757ba6774705d641834cb038175c32fbbd
seanlabor <50588881+seanlabor@users.noreply.github.com>
2025-05-04 12:15:47 +0200

    a

--- prepare-config-and-deploy.sh
+++ prepare-config-and-deploy.sh
@@ -4,7 +4,6 @@ echo ">> Preparing SolarGather config.yaml with environment variables..."
 export $(cat .env | xargs)
 envsubst < solargather_SG12RG/config.template.yaml > solargather_SG12RG/config.yaml
 envsubst < solargather_SG4RG/config.template.yaml > solargather_SG4RG/config.yaml
-envsubst < influxdb\init\01_sungather_bucket.template.sh > influxdb\init\01_sungather_bucket.sh
 
 
 # ensure all host config dirs exist
